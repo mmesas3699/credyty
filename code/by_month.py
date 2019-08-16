58d248a240653e3a78d3ef74e7f34f290bd79cc9
Miguel Mesa <mmesas369@gmail.com>
2019-08-15 22:31:32 -0500

    Genero la solución por mes en el archivo by_month.py

---
+++ code/by_month.py
@@ -0,0 +1,22 @@
+
+import calendar
+
+
+def count_sundays():
+    """Cuenta el total de ocaciones que un mes empieza en domingo
+    por cada año del siglo XX y retorna el resultado"""
+    total = 0
+    for year in range(1901, 2001):
+        for month in range(1, 13):
+            day = calendar.monthrange(year, month)[0]
+
+            if day == 6:
+                print(year, month)
+                total += 1
+
+    print('Total: ', total)
+    return total
+
+
+if __name__ == '__main__':
+    count_sundays()
